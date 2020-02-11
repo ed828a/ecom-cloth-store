8ae3f28292109aae11a132e35e9801a6a49797d7
ed828a <ed828a@gmail.com>
2020-02-11 11:27:57 +1030

    data normalization on shop data and change corresponding components

--- src/component/collections-overview/CollectionsOverview.jsx
+++ src/component/collections-overview/CollectionsOverview.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import CollectionPreview from "../../component/collection-preview/CollectionPreview";
-import { selectShopCollections } from "../../redux/shop/shop.selector";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selector";
 
 function CollectionsOverview({ collections }) {
     return (
@@ -16,7 +16,7 @@ function CollectionsOverview({ collections }) {
 }
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectShopCollections
+    collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
