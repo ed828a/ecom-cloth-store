577f12955d1492349af6e4146da2dec50105dfd7
ed828a <ed828a@gmail.com>
2020-02-21 14:47:44 +1030

    use variables

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -9,27 +9,27 @@ import firebase from 'firebase/app';
 import 'firebase/firestore'; 
 import 'firebase/auth';
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_API_KEY,
-//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//   databaseURL: process.env.REACT_APP_DATA_BASE_URL,
-//   projectId: process.env.REACT_APP_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_APP_ID,
-//   measurementId: process.env.REACT_APP_MEASUREMENT_ID
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCDU1xBiX07iW74fk-Hle9TM7oF2GwXO-E",
-  authDomain: "webdev-c9039.firebaseapp.com",
-  databaseURL: "https://webdev-c9039.firebaseio.com",
-  projectId: "webdev-c9039",
-  storageBucket: "webdev-c9039.appspot.com",
-  messagingSenderId: "1055330796052",
-  appId: "1:1055330796052:web:9ec8b4ca42650286589d01",
-  // measurementId: "G-7SNP0LP9KW"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATA_BASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  // measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCDU1xBiX07iW74fk-Hle9TM7oF2GwXO-E",
+//   authDomain: "webdev-c9039.firebaseapp.com",
+//   databaseURL: "https://webdev-c9039.firebaseio.com",
+//   projectId: "webdev-c9039",
+//   storageBucket: "webdev-c9039.appspot.com",
+//   messagingSenderId: "1055330796052",
+//   appId: "1:1055330796052:web:9ec8b4ca42650286589d01",
+//   // measurementId: "G-7SNP0LP9KW"
+// };
 
 
 export const createUserProfileDocument = async (userAuth, addtionalData) => {
