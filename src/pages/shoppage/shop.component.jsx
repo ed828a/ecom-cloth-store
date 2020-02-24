eb54eea37d9c0e8b39fe9cf243616539e5a763e5
ed828a <ed828a@gmail.com>
2020-02-24 22:08:02 +1030

    equip directory and shop with redux

--- src/pages/shoppage/shop.component.jsx
+++ src/pages/shoppage/shop.component.jsx
@@ -1,31 +1,24 @@
+import React from "react";
+import CollectionPreview from "../../component/collection-preview/collection-preview.component";
 
-import React, { Component } from 'react'
-import SHOP_DATA from '../../redux/shop/shop.data'
-import CollectionPreview from '../../component/collection-preview/collection-preview.component'
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectCollections } from "../../redux/shop/shop.selectors";
 
-export class ShopPage extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-             collections: SHOP_DATA
-        }
-    }
-    
-    render() {
-        const { collections } = this.state;
-        return (
-            <div className='shop-page'>
-               {
-                   Object.keys(collections)
-                   .map(key => collections[key])
-                   .map(({id, ...restCollectionProps}) => (
-                       <CollectionPreview key={id} {...restCollectionProps} />
-                   ))
-               }
-            </div>
-        )
-    }
-}
+export const ShopPage = ({ collections }) => {
+    return (
+        <div className="shop-page">
+            {Object.keys(collections)
+                .map(key => collections[key])
+                .map(({ id, ...restCollectionProps }) => (
+                    <CollectionPreview key={id} {...restCollectionProps} />
+                ))}
+        </div>
+    );
+};
 
-export default ShopPage
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+});
+
+export default connect(mapStateToProps)(ShopPage);
