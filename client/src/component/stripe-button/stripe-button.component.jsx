de87e1d495796768f061ec47053dada3fc0a3e4e
ed828a <ed828a@gmail.com>
2020-03-12 03:01:46 +1030

    add client payment charrge functionality

--- client/src/component/stripe-button/stripe-button.component.jsx
+++ client/src/component/stripe-button/stripe-button.component.jsx
@@ -1,22 +1,45 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
 import { clearAll } from "../../redux/cart/cart.actions";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
 
 function StripeCheckoutButton({ price, clearAll, history }) {
     const priceForStripe = price * 100;
     const publishableKey = process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY;
+    const [isPaymentSuccess, setIsPaymentSuccess] = useState(false);
 
     const onToken = token => {
-        console.log(token);
-        alert("Payment is successful.");
+        // console.log('token: ', token);
+        axios({
+            url: "/payment",
+            method: "post",
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        })
+            .then(response => {
+                alert("Payment is successful !, Thanks ");
+                setIsPaymentSuccess(true);
+            })
+            .catch(error => {
+                setIsPaymentSuccess(false);
+                console.error("Payment error: ", error);
+                alert(
+                    "There was an issue with your payment, please sure that you use the provided credit card."
+                );
+            });
     };
 
     const onClose = () => {
-        clearAll();
-        history.push('/');
-    }
+        if (isPaymentSuccess) {
+            clearAll();
+            history.push("/");
+        }
+    };
 
     return (
         <StripeCheckout
@@ -38,6 +61,8 @@ function StripeCheckoutButton({ price, clearAll, history }) {
 
 const mapDispatchToProps = dispatch => ({
     clearAll: () => dispatch(clearAll())
-})
+});
 
-export default withRouter( connect(null, mapDispatchToProps) (StripeCheckoutButton));
+export default withRouter(
+    connect(null, mapDispatchToProps)(StripeCheckoutButton)
+);
--- server.js
+++ server.js
@@ -1,32 +1,24 @@
-// entry point of the server code
-// import libraries in the way before ES6
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const path = require('path'); // 'path' gets bundled into any node project by default. it's a native module. what this module path does is that it lets us build out pathing for our directories.
+const path = require('path'); 
 
-if (process.env.NODE_ENV !== 'production') require('dotenv').config();  // this loads .env into our process environment which allows our process.env to access that secret key.
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();  
 
 // this line must below the above line, because it needs the secret key
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-const app = express(); // express is just a library that allows us to build an API server easily. this instance is a new expression application.
+const app = express(); 
 const port = process.env.PORT || 5000; // localhost is on 3000, and server is on 5000
 
-// we're gonna pass in bodyparser.json which means we're gonna make sure that any of the requests coming in, I want the server to process their body tag and convert ti to Jason so we can use it.
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); // make sure the url is understandable for URL 
-app.use(cors()); // cors stands for cross origin requests, what this does is if you think about it like this: our web server is being hosted from some origin(origin means like place, or port). when the front-end makes a request to the back-end, cors by default is to check to make sure the origin is the same. if it's not the same, then it dennies the request which is a safty feature.
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(cors()); 
 
-// we're hosting our own web server: be able to serve all of the static files in our build, by the following:
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static( // static: allows us to serve a certain file inside of input url path
-        path.join(__dirname, 'client/build')   // tell us what directory we're currently in and then we are pointing to client/build    
-    ));
+    app.use(express.static( path.join(__dirname, 'client/build')));
    
-    app.get('*', // * means any url, get means get-request
-    function(req, res){ // req: actual request we get.
-        // for any url request, we send the index.html file back to the front end.
+    app.get('*', function(req, res){ 
         res.sendFile(path.join(path.join(__dirname, 'client/build', 'index.html')));
     });
 }
@@ -36,19 +28,22 @@ app.listen(port, error => {
     console.log('Server running on port ' + port);
 });
 
-// build /payment route:
+
 app.post('/payment', (req, res) => {
+    console.log('server receive post request.');
     const body = {
         source: req.body.token.id,
         amount: req.body.amount,
         currency: 'aud'
     };
 
+    console.log('req: ', req); 
+    
     stripe.charges.create(body, (stripeErr, stripeRes) => {
         if (stripeErr){
             res.status(500).send({ error: stripeErr });
         } else {
             res.status(200).send({ sucess: stripeRes });
         }
-    })
+    });
 });
