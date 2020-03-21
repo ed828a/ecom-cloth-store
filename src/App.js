0569b38edbea7334e00cc70c0ec046f0f5b5673b
ed828a <ed828a@gmail.com>
2020-03-21 17:15:08 +1030

    Dynamically loading page components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React, { useEffect } from 'react'
-import './App.css';
+import React, { useEffect, lazy, Suspense } from 'react'
+// import './App.css';
 
-import HomePage from './pages/homepage/homepage.component';
+// import HomePage from './pages/homepage/homepage.component';
 import { Route, Switch, Redirect } from 'react-router-dom'
 import ErrorPage from './pages/errorpage/ErrorPage';
-import ShopPage from './pages/shoppage/shop.component';
+// import ShopPage from './pages/shoppage/shop.component';
 import Header from './component/header/header.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up-page/sign-in-and-sign-up.component';
+// import SignInAndSignUpPage from './pages/sign-in-and-sign-up-page/sign-in-and-sign-up.component';
 import { connect } from 'react-redux';
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { createStructuredSelector } from 'reselect';
-import CheckoutPage from './pages/checkoutpage/checkout.component';
+// import CheckoutPage from './pages/checkoutpage/checkout.component';
 import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
 import { checkUserSession } from './redux/user/user.action';
+import Spinner from './component/spinner/spinner.component';
+import { GlobalStyle } from './global.styles';
+
+const HomePage = lazy(() => import('./pages/homepage/homepage.component'));
+const ShopPage = lazy(() => import('./pages/shoppage/shop.component'));
+const CheckoutPage = lazy(() => import('./pages/checkoutpage/checkout.component'));
+const SignInAndSignUpPage = lazy(() => import('./pages/sign-in-and-sign-up-page/sign-in-and-sign-up.component'));
 
 
 export const App = ({ currentUser, checkUserSession }) => {
@@ -21,12 +28,17 @@ export const App = ({ currentUser, checkUserSession }) => {
 
   return (
     <div className="App">
+      <GlobalStyle />
       <Header />
       <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route exact path='/checkout' component={CheckoutPage} />
-        <Route exact path='/signin' render={() => currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
+        <Suspense fallback={<Spinner />} >
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
+          <Route exact path='/signin' render={() => currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
+        </Suspense>
+
+
         <Route component={ErrorPage} />
       </Switch>
 
