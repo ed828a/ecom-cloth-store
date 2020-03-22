8ebd59e5f4dd1f5119e70ef39d3522ad42a6036a
ed828a <ed828a@gmail.com>
2020-03-22 17:25:59 +1030

    learn React.Profiler

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,16 +1,22 @@
 // import './homepage.scss';
-import React from 'react';
-import Directory from '../../component/directory/directory.component';
-import { HomePageContainer } from './homepage.styles';
-
+import React, { Profiler } from "react";
+import Directory from "../../component/directory/directory.component";
+import { HomePageContainer } from "./homepage.styles";
 
 function HomePage() {
     // throw Error;
     return (
-        <HomePageContainer className='homepage'>
-            <Directory />
+        <HomePageContainer className="homepage">
+            <Profiler
+                id="directory"
+                onRender={(id, phase, actualDuration) => {
+                    console.log({ id, phase, actualDuration });
+                }}
+            >
+                <Directory />
+            </Profiler>
         </HomePageContainer>
-    )
+    );
 }
 
 export default HomePage;
