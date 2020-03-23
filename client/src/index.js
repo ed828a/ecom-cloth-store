682b7ea0d578a9100e5e596ed4869dbfc9a1611d
ed828a <ed828a@gmail.com>
2020-03-23 19:26:28 +1030

    small change

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import * as serviceWorker from './serviceWorker'; // import everything from serviceWorker.js
 
 
 ReactDOM.render(
@@ -18,4 +19,7 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root'));
 
-
+serviceWorker.register(); // register serviceWorker inside of our app for PWA
+// because we're using an express server, whenever the application load the serviceWorker, it's going to be looking for a serviceWorker.js file.
+// in our server, whenever the app makes a GET request for serviceWorker, we give them the serviceWorker.js file
+// inside server.js
