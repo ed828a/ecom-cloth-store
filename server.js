4819504ae55ffc8f9b793f1d67e21ecb1586fbc7
ed828a <ed828a@gmail.com>
2020-03-24 00:28:26 +1030

    add PWA requirement files

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path'); 
 const compression = require('compression');
+const enforce = require('express-sslify');
+
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();  
 
@@ -16,6 +18,7 @@ app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(cors()); 
+app.use(enforce.HTTPS({trustProtoHeader: true}));
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static( path.join(__dirname, 'client/build')));
@@ -30,6 +33,9 @@ app.listen(port, error => {
     console.log('Server running on port ' + port);
 });
 
+app.get('/service-worker.js', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+} )
 
 app.post('/payment', (req, res) => {
     console.log('server receive post request.');
